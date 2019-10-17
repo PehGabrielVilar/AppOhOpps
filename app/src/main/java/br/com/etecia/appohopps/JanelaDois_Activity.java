0004644225acddeff5b2d1545f356c842e7f9fb0
Pedro Gabriel Vilar <pedro.santos589@etec.sp.gov.br>
2019-10-17 00:26:28 -0300

    APPRONTO

---
+++ app/src/main/java/br/com/etecia/appohopps/JanelaDois_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appohopps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JanelaDois_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.janeladois_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/appohopps/JanelaTres_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appohopps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JanelaTres_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.janelatres_layout);
+    }
+}
